ffbd8ec39601c189ad12f921f3ddeb60bc06e4f2
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2022-07-09 16:13:11 +0900

    Add device check at start up

--- co2meter_exporter/__main__.py
+++ co2meter_exporter/__main__.py
@@ -1,4 +1,8 @@
 import argparse
+from logging import getLogger
+import os
+import stat
+import sys
 import time
 
 from CO2Meter import CO2Meter
@@ -7,6 +11,8 @@ from prometheus_client.core import REGISTRY
 
 from co2meter_exporter.collector import CustomCollector
 
+
+_LOGGER = getLogger(__name__)
 
 parser = argparse.ArgumentParser(
     description='Prometheus exporter for CO2 meter')
@@ -19,13 +25,18 @@ parser.add_argument('--port', type=int, default=9817,
 args = parser.parse_args()
 
 device = args.device
+
+if not stat.S_ISCHR(os.lstat(device).st_mode):
+    _LOGGER.error("Device is not a character special device: %s", device)
+    sys.exit(1)
+
 meter = CO2Meter(device)
 REGISTRY.register(CustomCollector(meter))
-print("Using CO2Meter at %s" % device)
+_LOGGER.info("Using CO2Meter at %s", device)
 
 host = args.host
 port = args.port
 start_http_server(port, host)
-print("Listening on %s:%d" % (host, port))
+_LOGGER.info("Listening on %s:%d", host, port)
 while True:
     time.sleep(1)
